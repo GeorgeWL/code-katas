f04e65e8dc60b5565ce240866684c46d10e1a9e1
GeorgeWL <george.langham@ivendi.com>
2020-01-13 13:48:35 +0000

    add linter and tsconfig

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import rgb from './hexToRgb';
-import isIsogram from './isogram_check';
-console.log('will add proper jest tests here later');
-console.assert(rgb(0, 0, 0) === '000000', 'FAIL on 0,0,0');
-console.assert(rgb(0, 0, -20) === '000000', 'FAIL');
-console.assert(rgb(300, 255, 255) === 'FFFFFF', 'FAIL');
-console.assert(rgb(173, 255, 47) === 'ADFF2F', 'FAIL');
-// console.assert(
-// 	isIsogram('Dermatoglyphics') === true,
-// 	'definitely an isogram word'
-// );
-// console.assert(isIsogram('isogram') === true);
-// console.assert(isIsogram('aba') === false, 'same chars may not be adjacent');
-// console.assert(isIsogram('moOse') === false, 'same chars may not be same case');
-// console.assert(isIsogram('isIsogram') === false);
-// console.assert(isIsogram('') === true, 'an empty string is a valid isogram');
+import rgb from "./hexToRgb";
+import isIsogram from "./isogram_check";
+console.log("will add proper jest tests here later");
+console.assert(rgb(0, 0, 0) === "000000", "FAIL on 0,0,0");
+console.assert(rgb(0, 0, -20) === "000000", "FAIL");
+console.assert(rgb(300, 255, 255) === "FFFFFF", "FAIL");
+console.assert(rgb(173, 255, 47) === "ADFF2F", "FAIL");
+console.assert(
+    isIsogram("Dermatoglyphics") === true,
+    "definitely an isogram word"
+);
+console.assert(isIsogram("isogram") === true);
+console.assert(isIsogram("aba") === false, "same chars may not be adjacent");
+console.assert(isIsogram("moOse") === false, "same chars may not be same case");
+console.assert(isIsogram("isIsogram") === false);
+console.assert(isIsogram("") === true, "an empty string is a valid isogram");
