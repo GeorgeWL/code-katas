8088c464cf5140b29927cffe495b04a14d6c1f44
GeorgeWL <george.langham@ivendi.com>
2020-01-13 12:25:42 +0000

    add notes. update packagejson

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-console.log(`Hey Folks 👋, was gonna add a Commander (npmjs.org/package/commander)
- here that would run tests with cli, but couldn't be bothered
- will likely add one at a later date`);
+import rgb from './hexToRgb';
+import isIsogram from './isogram_check';
+console.log('will add proper jest tests here later');
+console.assert(rgb(0, 0, 0) === '000000', 'FAIL on 0,0,0');
+console.assert(rgb(0, 0, -20) === '000000', 'FAIL');
+console.assert(rgb(300, 255, 255) === 'FFFFFF', 'FAIL');
+console.assert(rgb(173, 255, 47) === 'ADFF2F', 'FAIL');
+// console.assert(
+// 	isIsogram('Dermatoglyphics') === true,
+// 	'definitely an isogram word'
+// );
+// console.assert(isIsogram('isogram') === true);
+// console.assert(isIsogram('aba') === false, 'same chars may not be adjacent');
+// console.assert(isIsogram('moOse') === false, 'same chars may not be same case');
+// console.assert(isIsogram('isIsogram') === false);
+// console.assert(isIsogram('') === true, 'an empty string is a valid isogram');
--- isogram_check.ts
+++ isogram_check.ts
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/54ba84be607a92aa900000f1
-function wordIsIsogram(word: string): boolean {
+export default function isIsogram(word: string): boolean {
 	const lettersArr: string[] = word
 		.toLowerCase()
 		.split('')
