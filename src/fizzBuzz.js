24391a035c4a0fd5f7269481ad069f618da3626e
George W Langham (desktop) <georgelangham1990@gmail.com>
2021-04-21 18:02:29 +0100

    simple fizzbuzz

---
+++ src/fizzBuzz.js
@@ -0,0 +1,24 @@
+// simple so shouldn't need tests
+
+export default function fizzBuzzer(maxCount = 100) {
+    // add 1 to maxCount as javascript is zero-indexed
+    for (let index = 1; index < maxCount+1; index++) {
+        const isThreeMult = isMultipleOfX(index, 3);
+        const isFiveMult = isMultipleOfX(index, 5);
+        const message = isThreeMult || isFiveMult ? `${isThreeMult ? 'Fizz' : ''}${isFiveMult ? 'Buzz' : ''}` : index;
+        console.log(message);
+    }
+}
+function isMultipleOfX(int, multiple) {
+    // simple type checks
+    if (typeof int != 'number') {
+        throw TypeError("int parameter must be a number")
+    }
+    if (typeof multiple != 'number') {
+        throw TypeError("multiple parameter must be a number")
+    }
+    if (multiple <= 0) {
+        throw new Error("multiple must be larger than 0, cannot divide by zero")
+    }
+    return int % multiple == 0;
+}
